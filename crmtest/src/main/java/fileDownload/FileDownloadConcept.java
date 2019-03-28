abfbfaeaed55a9ba2046e19c6cf2567105491870
Quang <qtvo91@yahoo.com>
2019-03-27 21:24:07 -0700

    The POM.xml file has the correct dependency for Skuli API version.

--- crmtest/src/main/java/fileDownload/FileDownloadConcept.java
+++ crmtest/src/main/java/fileDownload/FileDownloadConcept.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
